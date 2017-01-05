87221553ff99efafbcb44d1c98abca32ee887771
Suhail Idrees <gusmanbhai@hotmail.com>
2017-01-05 12:08:26 -0800

    Integrate Map/Autocomplete views into wireframes.  Add confirm location button to Map view.

--- screens/ChooseLocationScreen.js
+++ screens/ChooseLocationScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {
-  ScrollView,
-  StyleSheet,
-  Text
-} from 'react-native';
+import {Button, View} from 'react-native';
+import { Components } from 'exponent';
+import Router from '../navigation/Router';
+import config from '../config';
+var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
+
 
 export default class ChooseLocationScreen extends React.Component {
   static route = {
@@ -12,19 +13,68 @@ export default class ChooseLocationScreen extends React.Component {
     },
   }
 
-  render() {
-        // style={styles.container} this is causing the top bar to not render consistently
-    return (
-      <ScrollView
-        contentContainerStyle={this.props.route.getContentContainerStyle()}>
-        <Text>Map goes here</Text>
-      </ScrollView>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      latlng: {},
+    };
   }
 
-}
 
-const styles = StyleSheet.create({
-  container: {
-  },
-});
+  render() {
+    return (
+      <GooglePlacesAutocomplete
+        placeholder='Search'
+        minLength={2} // minimum length of text to search
+        autoFocus={false}
+        listViewDisplayed='auto'    // true/false/undefined
+        fetchDetails={true}
+
+        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+          // console.log(data);
+          // console.log(details.geometry.location);
+          let location = {
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+          };
+          this.props.navigator.push(Router.getRoute('map', {location: location}))
+
+        }}
+        getDefaultValue={() => {
+          return ''; // text input default value
+        }}
+        query={{
+          // available options: https://developers.google.com/places/web-service/autocomplete
+          key: config.googleAPIKey,
+          language: 'en', // language of the results
+          types: '(cities)', // default: 'geocode'
+        }}
+        styles={{
+          description: {
+            fontWeight: 'bold',
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+
+        }}
+
+        currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+        currentLocationLabel="Current location"
+        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+        GoogleReverseGeocodingQuery={{
+          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+        }}
+        GooglePlacesSearchQuery={{
+          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+          rankby: 'distance',
+          types: 'food',
+        }}
+
+
+        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+
+      />
+    );
+  } 
+}
