6bdbf5f9189a7f87abc6c3060ed7137cca9ffcbb
Anton <anton.arboleda2gmail.com>
2017-01-16 10:52:52 -0800

    Add eslintrc file for style guide into project

--- actions/dishActions.js
+++ actions/dishActions.js
@@ -1,21 +1,21 @@
 module.exports = {
   updateName: function (name) {
-    return {type: 'UPDATE_NAME', name};
+    return {type: 'UPDATE_DISH_NAME', name};
   },
   updateText: function (text) {
-    return {type: 'UPDATE_TEXT', text};
+    return {type: 'UPDATE_DISH_TEXT', text};
   },
   updatePrice: function (price) {
-    return {type: 'UPDATE_PRICE', price};
+    return {type: 'UPDATE_DISH_PRICE', price};
   },
   updateRestrictions: function (restrictions) {
-    return {type: 'UPDATE_RESTRICTIONS', restrictions};
+    return {type: 'UPDATE_DISH_RESTRICTIONS', restrictions};
   },
   updateCuisine: function (cuisines) {
-    return {type: 'UPDATE_CUISINES', cuisines};
+    return {type: 'UPDATE_DISH_CUISINES', cuisines};
   },
   updateDish: function (dish) {
-    return {type: 'UPDATE_DISH', dish};
+    return {type: 'UPDATE_ENTIRE_DISH', dish};
   },
   deleteDish: function () {
     return {type: 'REMOVE_DISH'};
