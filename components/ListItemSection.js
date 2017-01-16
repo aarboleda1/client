52f0443dff11c4e2803035217d17affa03bc5392
Anton <anton.arboleda2gmail.com>
2017-01-16 00:53:24 -0800

    Add modals for choosing fields for dishes

--- components/ListItemSection.js
+++ components/ListItemSection.js
@@ -15,7 +15,6 @@ const ListItemSection = (props) => {
 const styles = {
   containerStyle: {
     borderBottomWidth: 1,
-    padding: 5,
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
     flexDirection: 'row',
--- screens/CreateDishScreen.js
+++ screens/CreateDishScreen.js
@@ -8,7 +8,10 @@ import {
   AsyncStorage,
   Text,
   TextInput,
-  Image
+  Image,
+  Modal,
+  ActivityIndicator
+
 } from 'react-native';
 
 
@@ -21,26 +24,94 @@ import ListItemSection from '../components/ListItemSection';
 class CreateDishScreen extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+    };
   }
   static route = {
     navigationBar: {
-      title: ' Dish',
+      title: 'Create New Dish',
     },
   }
 
+  toggleState(key) {
+    let update = {};
+    update[key] = !this.state[key];
+    this.setState(update);
+  }
   render () {
     return (
       <ScrollView style={styles.flex}>
-        <Image 
-          source={{uri: this.props.dishes.dishList[0].image}}
-          style={styles.imageStyle}
-        />
+        <ListItemSection>
+          <Image 
+            source={{uri: "http://i0.wp.com/wrbbradio.org/wp-content/uploads/2016/10/grey-background-07.jpg?zoom=2&fit=2560%2C1544"}}
+            style={styles.imageStyle}
+            backGroundColor
+          />
+        </ListItemSection>
+        <ListItem>
+          <Button 
+            title="Give Dish a Title"
+            onPress={this.toggleState.bind(this, 'showAddTitleModal')}
+          />
+        </ListItem>
+        <ListItem>
+          <Button 
+            title="Describe Your Dish"
+            onPress={this.toggleState.bind(this, 'describeDishModal')}
+          />
+        </ListItem>
+        <ListItem>
+          <Button 
+            title="Set Price"
+            onPress={this.toggleState.bind(this, 'setPriceModal')}
+          />
+        </ListItem>        
+        <ListItem>
+          <Button 
+            title="Dietary Restrictions this may meet"
+            onPress={this.toggleState.bind(this, 'setDietaryRestrictionsModal')}
+          />
+        </ListItem>        
+        <ListItem>
+          <Button 
+            title="Set Cuisines"
+            onPress={this.toggleState.bind(this, 'setCuisinesModal')}
+          />
+        </ListItem>
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={!!this.state.showAddTitleModal}> 
+        </Modal>
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={!!this.state.describeDishModal}> 
+        </Modal>
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={!!this.state.setDietaryRestrictionsModal}> 
+        </Modal>
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={!!this.state.setPriceModal}> 
+        </Modal>        
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={!!this.state.setCuisinesModal}> 
+        </Modal>
       </ScrollView>
     ) 
   };
 }
 
 const styles = {
+  flex: {
+    flex: 1,
+  },
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
@@ -57,6 +128,10 @@ const styles = {
   imageContainerStyle: {
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  formInput: {
+    flex: 1,
+    height: 30,
   },
 };
 
