72e5252f898c4f1b3168902503d07886e1d6dbe1
Anton <anton.arboleda2gmail.com>
2017-01-18 16:43:02 -0800

    Add functionality for adding dishes

--- helpers/dishHelpers.js
+++ helpers/dishHelpers.js
@@ -2,6 +2,7 @@ import { serverURI } from '../config';
 import AsyncStorage from 'react-native';
 module.exports = {
   postDishToDB: function (newDish, currentUserId) {
+    console.log(newDish.image, 'is the image when posting to DB');
     return fetch(`${serverURI}/dishes/chefs/${currentUserId}`, {
       method: 'POST',
       headers: {
